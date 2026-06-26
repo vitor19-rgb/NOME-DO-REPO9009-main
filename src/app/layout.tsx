8bd9ec656375ba8ea6a2cbb95267fbb6893156ff
vitor <vitrola445@gmail.com>
2026-06-26 08:17:40 -0300

    teste 67

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/IconePlaneta2.png', sizes: '192x192', type: 'image/png' },
-      { url: '/IconePlaneta2.png', sizes: '512x512', type: 'image/png' },
+      { url: '/icon.png', sizes: '512x512', type: 'image/png' },
     ],
-    shortcut: ['/IconePlaneta2.png'],
+    shortcut: ['/icon.png'],
     apple: [
       { url: '/IconePlaneta2.png', sizes: '180x180', type: 'image/png' },
     ],
